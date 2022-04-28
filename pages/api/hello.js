82dc2ca8a4a00b3268782686a21528ca50506cd4
PPadilla44 <Padilla.pablo1998@gmail.com>
2022-04-28 14:09:29 -0700

    prisma init

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const { PrismaClient } = require('@prisma/client')
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const prisma = new PrismaClient()
+export default async function handler(req, res) {
+  try {
+    const createdUSer = await prisma.user.create({
+      data: {
+        userName: "fasmish1",
+        email: "famish1@gmail.com",
+        password: "pass"
+      }
+    })
+    const allUsers = await prisma.user.findMany();
+    res.status(200).json(allUsers)
+  } catch (err) {
+    res.status(400).json({ MSG: "NO" })
+  }
 }
