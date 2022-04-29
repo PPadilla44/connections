fd3562a20c19e44083d8fe061e7fcf6173bfc09a
Spencer Rauch <rauchs@gmail.com>
2022-04-29 10:05:09 -0700

    level creator first steps

---
+++ pages/level_creator.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const level_creator = () => {
+  return (
+    <div className="bg-blue-200 h-screen flex relative">
+      <div className="bg-orange-200 h-[645px] w-[1200px]"></div>
+      </div>
+  )
+}
+
+export default level_creator
