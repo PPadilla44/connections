caacd5aa6b32cc7c80b77418a8543b709feed0de
Spencer Rauch <rauchs@gmail.com>
2022-04-29 10:47:18 -0700

    level creator page basic functionality

--- pages/level_creator.jsx
+++ pages/level_creator.jsx
@@ -1,12 +1,42 @@
 import React from 'react'
+import {useState} from 'react'
 
+export default function level_creator() {
+  const [dots, setDots] = useState({})
+  const [i, setI] = useState(1)
 
-const level_creator = () => {
+  const clickHandler = (e) => {
+    let temp = dots;
+    temp[i] = {x: e.pageX, y:e.pageY}
+    setDots(temp)
+    setI(++i)
+  }
+
+  const undo = () => {
+    let toRemove = i - 1
+    let temp = dots;
+    delete temp[toRemove]
+    console.log(temp)
+    if (i > 1){
+      setI(--i)
+    }
+    setDots(temp)
+
+  }
+
   return (
     <div className="bg-blue-200 h-screen flex relative">
-      <div className="bg-orange-200 h-[645px] w-[1200px]"></div>
-      </div>
+      <div onClick={clickHandler} className="bg-orange-200 h-[645px] w-[1200px]"></div>
+      {Object.entries(dots).map((p) => (
+      <button
+        key={p[0]}
+        id={p[0]}
+        className={`active:bg-red-600 z-20 bg-black h-4 w-4 absolute rounded-full`}
+        style={{ left: p[1].x, top: p[1].y }}>
+        <span className="absolute top-3">{p[0]}</span>
+      </button>
+    ))}
+    <button onClick={undo}>UNDO</button>
+    </div>
   )
 }
-
-export default level_creator
